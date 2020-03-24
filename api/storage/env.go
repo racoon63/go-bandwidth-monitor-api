7c422d26d9c904921aa4cc698ff1af92f5e69ac2
Patrick Lubach <p.lubach@reply.de>
2020-03-24 21:08:19 +0100

    init code

---
+++ api/storage/env.go
@@ -0,0 +1,20 @@
+package storage
+
+import (
+	"os"
+)
+
+// Envars struct holds all environment values
+type Envars struct {
+	Backend  string
+	Username string
+	Password string
+}
+
+func readEnvars() Envars {
+	var e Envars
+	e.Backend = os.Getenv("BACKEND")
+	e.Username = os.Getenv("USERNAME")
+	e.Password = os.Getenv("PASSWORD")
+	return e
+}
